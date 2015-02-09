69e9c61d2400ade5e98d80bdf3eb51e53d6eebfb
Lorena Azuara <Lore@MacBook-Air-de-Lore.local>
2015-02-09 00:17:00 -0600

    commit inicial

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Lore
+ *
+ * Created on 9 de febrero de 2015, 12:12 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
